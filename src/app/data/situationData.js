7c29e3fb5c28133872ba2c3065aad57ba143a641
sorimchuku <sorimchuku@gmail.com>
2025-06-26 05:31:56 +0000

    메인 롤링 애니메이션

--- src/app/data/situationData.js
+++ src/app/data/situationData.js
@@ -2,7 +2,7 @@ export const situation_data = [
   {
     "id": 1,
     "name": "",
-    "title": "그룹 면접장, 하필 내가 첫 번째 순서",
+    "title": "그룹 면접장, 하필 내가 첫 번째 순서!",
     "description": "자기소개 먼저 해주세요.",
     "time": 20,
     "textCount": 150,
@@ -11,8 +11,8 @@ export const situation_data = [
   {
     "id": 2,
     "name": "",
-    "title": "엘리베이터에서 임원과 단둘이",
-    "description": "어느 팀이야?",
+    "title": "한 번도 얘기 안해본 옆부서 상사와 엘리베이터에서 단둘이..",
+    "description": "이름이 뭐였죠?",
     "time": 15,
     "textCount": 100,
     "imageUrl": ""
@@ -20,8 +20,8 @@ export const situation_data = [
   {
     "id": 3,
     "name": "",
-    "title": "소개팅 상황, 주선자가 자기소개를 요청!",
-    "description": "자기소개 한번 해볼래?",
+    "title": "소개팅 상황, 주선자가 나를 소개한다",
+    "description": "그리고 이쪽은..",
     "time": 10,
     "textCount": 80,
     "imageUrl": ""
@@ -29,8 +29,8 @@ export const situation_data = [
   {
     "id": 4,
     "name": "",
-    "title": "친구의 권유로 들어간 팀, 첫 회의 시간",
-    "description": "자기소개 한 번 해주시죠.",
+    "title": "과학동아리에 신입부원으로서 첫날!",
+    "description": "신입부원이신데 소개부탁드려요",
     "time": 15,
     "textCount": 120,
     "imageUrl": ""
@@ -38,8 +38,8 @@ export const situation_data = [
   {
     "id": 5,
     "name": "",
-    "title": "회사 워크숍, TMI 포함 자기소개",
-    "description": "자기 이름 + TMI 하나!",
+    "title": "신입으로 들어간 회사, 바로 워크샵에 가게 되었다!",
+    "description": "여기 신입 분, 소개부탁드려요!",
     "time": 10,
     "textCount": 80,
     "imageUrl": ""
@@ -47,8 +47,8 @@ export const situation_data = [
   {
     "id": 6,
     "name": "",
-    "title": "공연을 보러갔다. 그런데 갑자기 나에게 마이크가...!",
-    "description": "자기소개 한 마디 해주시죠.",
+    "title": "공개방송 방청을 갔는데 갑자기 나에게 마이크가..!",
+    "description": "여기 이 분! 소개 부탁드려요~",
     "time": 10,
     "textCount": 80,
     "imageUrl": ""
@@ -56,8 +56,8 @@ export const situation_data = [
   {
     "id": 7,
     "name": "",
-    "title": "개강총회, 처음 보는 동기들과 같은 테이블에 앉게 됐다!",
-    "description": "혹시 누구니?",
+    "title": "첫 개강총회, 낯선 동기들과 같은 테이블에..",
+    "description": "혹시 2n학번이세요?",
     "time": 15,
     "textCount": 120,
     "imageUrl": ""
@@ -65,8 +65,8 @@ export const situation_data = [
   {
     "id": 8,
     "name": "",
-    "title": "러닝 동호회에 가입했다. 신입은 오랜만이라는데...",
-    "description": "어떻게 오시게 됐나요?",
+    "title": "러닝크루에 가입, 오랜만의 신입이라는데?",
+    "description": "어떻게 오시게 되셨나요?",
     "time": 10,
     "textCount": 100,
     "imageUrl": ""
@@ -74,8 +74,8 @@ export const situation_data = [
   {
     "id": 9,
     "name": "",
-    "title": "갑작스런 SNS 라이브 출연",
-    "description": "간단히 자기소개 해주세요.",
+    "title": "실시간 방송 중인 유튜버가 다가왔다!",
+    "description": "잠깐 인터뷰 가능하세요? 소개좀..",
     "time": 15,
     "textCount": 120,
     "imageUrl": ""
@@ -83,8 +83,8 @@ export const situation_data = [
   {
     "id": 10,
     "name": "",
-    "title": "조직개편으로 새로운 팀원들과 일하게 됐다!",
-    "description": "혹시 소개...",
+    "title": "조직개편으로 새로운 부서에 가게 되었다!",
+    "description": "여긴 마케팅1팀에서 온.. 소개해줘요",
     "time": 10,
     "textCount": 80,
     "imageUrl": ""
@@ -92,8 +92,8 @@ export const situation_data = [
   {
     "id": 11,
     "name": "",
-    "title": "MT/OT 자기소개 + 이상형 미션",
-    "description": "자기소개랑 이상형 말해보자!",
+    "title": "MT에 왔다! 자기소개 + 이상형 말하기 미션",
+    "description": "자기소개랑 이상형 말해주기!!",
     "time": 20,
     "textCount": 150,
     "imageUrl": ""
@@ -101,8 +101,8 @@ export const situation_data = [
   {
     "id": 12,
     "name": "",
-    "title": "줌 회의 중 갑작스러운 언뮤트 요청",
-    "description": "음소거 풀고 자기소개해주세요.",
+    "title": "관심분야 줌 워크샵에 참여했다. 참여자들과 대화가 시작됐다.",
+    "description": "각자 소개부터 해볼까요?",
     "time": 15,
     "textCount": 120,
     "imageUrl": ""
@@ -110,8 +110,8 @@ export const situation_data = [
   {
     "id": 13,
     "name": "",
-    "title": "원룸 보러 가는 길, 공인중개사 아주머니가",
-    "description": "학생은 전공이 뭐야?",
+    "title": "방을 보러갔는데 공인중개사 아줌마가..",
+    "description": "학생이야? 전공은 뭐야?",
     "time": 10,
     "textCount": 80,
     "imageUrl": ""
@@ -119,8 +119,8 @@ export const situation_data = [
   {
     "id": 14,
     "name": "",
-    "title": "동아리 면접 초단시간 자기소개",
-    "description": "자기소개 5초 컷 해보세요.",
+    "title": "취미 밴드동아리 면접, 딱 5초, 소개해보세요!",
+    "description": "딱 5초만 인상 깊은 소개를 해주세요",
     "time": 5,
     "textCount": 40,
     "imageUrl": ""
@@ -128,8 +128,8 @@ export const situation_data = [
   {
     "id": 15,
     "name": "",
-    "title": "캠프 조 편성 전 자기소개",
-    "description": "한 명씩 짧게 소개해주세요!",
+    "title": "캠핑 체험 행사에 왔다. 같은 텐트를 쓸 짝을 찾으라는데?",
+    "description": "안녕하세요, 혹시 성함이..?",
     "time": 10,
     "textCount": 80,
     "imageUrl": ""
@@ -137,8 +137,8 @@ export const situation_data = [
   {
     "id": 16,
     "name": "",
-    "title": "클럽/스터디 첫 모임 소개 시간",
-    "description": "각자 뭐 하는 사람인지 말해주세요.",
+    "title": "토익스터디에 가입했다. 첫날인데..",
+    "description": "자기소개랑 포부 부탁드려요",
     "time": 15,
     "textCount": 120,
     "imageUrl": ""
@@ -146,8 +146,8 @@ export const situation_data = [
   {
     "id": 17,
     "name": "",
-    "title": "갑작스러운 전화 면접 시작",
-    "description": "지금 자기소개 가능할까요?",
+    "title": "갑자기 걸려온 전화면접",
+    "description": "자기 소개해주실 수 있을까요?",
     "time": 20,
     "textCount": 150,
     "imageUrl": ""
@@ -155,8 +155,8 @@ export const situation_data = [
   {
     "id": 18,
     "name": "",
-    "title": "지인 친구 모임에 따라갔다가 갑자기...",
-    "description": "이 친구 누구야?",
+    "title": "친구가 불러서 나갔는데 친구가 처음보는 사람을 데려왔다",
+    "description": "너네 친해져~ 너 소개 좀 해봐!",
     "time": 10,
     "textCount": 80,
     "imageUrl": ""
@@ -164,8 +164,8 @@ export const situation_data = [
   {
     "id": 19,
     "name": "",
-    "title": "택시 기사님의 스몰토크",
-    "description": "요즘 젊은 사람들은 뭐 해~?",
+    "title": "면접 가는 길, 택시 기사님이 말을 걸어온다",
+    "description": "어디 가는 길이야~? 무슨 일 해?",
     "time": 10,
     "textCount": 80,
     "imageUrl": ""
@@ -173,8 +173,8 @@ export const situation_data = [
   {
     "id": 20,
     "name": "",
-    "title": "공모전 발표 순서 변경",
-    "description": "1번 발표자님, 자기소개 부탁드려요.",
+    "title": "공모전 순서가 바뀌어 갑자기 발표 무대로 올라가게 되었다",
+    "description": "지원자님 자기소개 하고 시작해주세요",
     "time": 15,
     "textCount": 120,
     "imageUrl": ""
@@ -182,8 +182,8 @@ export const situation_data = [
   {
     "id": 21,
     "name": "",
-    "title": "새로운 동네로 이사, 이웃과 첫 대화",
-    "description": "안녕하세요 옆집인데요...",
+    "title": "이사떡을 돌리러 옆집 문을 두드렸다",
+    "description": "누구세요?",
     "time": 15,
     "textCount": 100,
     "imageUrl": ""
@@ -191,7 +191,7 @@ export const situation_data = [
   {
     "id": 22,
     "name": "",
-    "title": "비행기 옆자리 사람이 말을 걸어왔다",
+    "title": "혼자 떠나는 여행, 비행기 옆자리 사람이 말을 걸어온다!",
     "description": "여행 가시는 거예요?",
     "time": 15,
     "textCount": 100,
@@ -200,8 +200,8 @@ export const situation_data = [
   {
     "id": 23,
     "name": "",
-    "title": "심야 라디오 DJ와의 전화연결에 당첨!",
-    "description": "간단한 자기소개 부탁드려요~",
+    "title": "심야 라디오, 내가 보낸 사연이 당첨됐다!",
+    "description": "간단히~ 자기소개 해주세요~",
     "time": 10,
     "textCount": 80,
     "imageUrl": ""
@@ -209,8 +209,8 @@ export const situation_data = [
   {
     "id": 24,
     "name": "",
-    "title": "여행 중 호스텔에서 만난 여행객들과 자기소개",
-    "description": "어디서 왔어요?",
+    "title": "홀로 온 제주 게스트하우스, 다른 여행객과 마주쳤다",
+    "description": "혼자 오셨나요? 소개 좀 해주세요~",
     "time": 15,
     "textCount": 100,
     "imageUrl": ""
@@ -218,8 +218,8 @@ export const situation_data = [
   {
     "id": 25,
     "name": "",
-    "title": "독서 모임 첫 참여",
-    "description": "간단히 자기소개 부탁드려요.",
+    "title": "책 좀 읽으려고 참여한 철학 독서 모임 첫날..",
+    "description": "어떻게 오시게 됐나요?",
     "time": 15,
     "textCount": 120,
     "imageUrl": ""
--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,27 @@
 'use client';
 
 import Image from "next/image";
-import localFont from "next/font/local";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useGlobalContext } from "./context/globalContext";
 import { situation_data } from "./data/situationData";
 
-const ptab = localFont({
-  src: "./fonts/ptab.ttf",
-  variable: "--font-ptab",
-  weight: "400",
-});
-
 export default function Home() {
+  const initialAnimationSpeed = 500; // 초기 애니메이션 속도 설정
   const { situationData, setSituationData } = useGlobalContext();
   const [startPageOpen, setStartPageOpen] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [prevIndex, setPrevIndex] = useState(0);
+  const [isSliding, setIsSliding] = useState(true);
   const [finalIndex, setFinalIndex] = useState(-1);
   const [isAnimating, setIsAnimating] = useState(true);
-  const [animationSpeed, setAnimationSpeed] = useState(100);
+  const [animationSpeed, setAnimationSpeed] = useState(initialAnimationSpeed);
   const [responseType, setResponseType] = useState("voice");
   const router = useRouter();
+  const slideTimeout = useRef(null);
+
+
+  const imageUrlbase = (id, ext) => `/image/thumbnails/${ext}/situationScreenImage_${String(id).padStart(2, '0')}.${ext}`;
 
   const handleResponseButtonClick = (responseType) => {
     setIsAnimating(true);
@@ -30,19 +30,19 @@ export default function Home() {
     let speed = animationSpeed;
 
     const slowDownAnimation = () => {
-      if (speed > 500) {
+      if (speed > 1000) {
         const randomIndex = Math.floor(Math.random() * situation_data.length);
         setFinalIndex(randomIndex); // 최종 선택된 인덱스 설정
         setCurrentIndex(randomIndex); // 현재 인덱스 업데이트
         return;
       }
 
-      speed += 50;
+      speed += 100;
       setAnimationSpeed(speed);
 
       setTimeout(() => {
         const randomIndex = Math.floor(Math.random() * situation_data.length);
-        setCurrentIndex(randomIndex); // 현재 인덱스 업데이트
+        setCurrentIndex(randomIndex);
         slowDownAnimation();
       }, speed);
     };
@@ -50,12 +50,24 @@ export default function Home() {
     slowDownAnimation();
   };
 
+  // 이미지 변경 시 prevIndex 업데이트 및 슬라이드 트리거
+  useEffect(() => {
+    if (currentIndex !== prevIndex) {
+      setIsSliding(true);
+      if (slideTimeout.current) clearTimeout(slideTimeout.current);
+      slideTimeout.current = setTimeout(() => 
+        { setIsSliding(false);
+          setPrevIndex(currentIndex); // 이전 인덱스 업데이트
+        }, 300);
+    }
+  }, [currentIndex]);
+
   useEffect(() => {
     if (finalIndex !== -1) {
       setCurrentIndex(finalIndex);
       setSituationData(situation_data[finalIndex]); // 최종 선택된 데이터를 전역 상태에 저장
       setIsAnimating(false);
-      setAnimationSpeed(100);
+      setAnimationSpeed(initialAnimationSpeed);
 
       // 1초 후 페이지 이동
       setTimeout(() => {
@@ -79,17 +91,42 @@ export default function Home() {
     }
   }, [isAnimating, animationSpeed]);
 
+  const randomContainer = (
+    <div className="random-container relative w-full h-60 flex items-center justify-center overflow-hidden">
+      {/* 이전 이미지 */}
+        <Image
+          key={`prevIndex-${prevIndex}`}
+          src={imageUrlbase(situation_data[prevIndex].id, 'webp')}
+          alt="prev"
+          height={240}
+          width={240}
+          loading="eager"
+          unoptimized={true}
+          priority={true}
+          className={`object-contain absolute h-full w-auto rounded-lg z-10 border border-background-gray ${isSliding ? "animate-slide-out" : ""}`}
+        />
+      {/* 현재 이미지 */}
+      <Image
+        key={`currentIndex-${currentIndex}`}
+        src={imageUrlbase(situation_data[currentIndex].id, 'webp')}
+        alt="current"
+        height={240}
+        width={240}
+        loading="eager"
+        unoptimized={true}
+        priority={true}
+        className={`object-contain absolute w-auto h-full rounded-lg z-20 border border-background-gray ${isSliding ? "animate-slide-in" : ""}`}
+      />
+    </div>
+  );
+
 
 
   const introPage = (
     <>
     <div className="background-container absolute inset-0 flex items-center justify-center -z-100">
-      <Image src={"/image/background/main_temp.png"} alt="intro background png" layout="fill" objectFit="cover" className="object-cover" />
+      <Image src={"/image/background/main_temp.png"} alt="intro background png" fill className="object-cover" />
     </div>
-      {/* <h1 className={`title text-center text-5xl flex flex-col items-center justify-center my-auto gap-2 ${ptab.className}`}>
-        <div className="title-text">감자</div>
-        <div className="title-text">시뮬레이터</div>
-      </h1> */}
       <div className="button-group flex flex-col items-center justify-end h-full w-full grow pb-4">
         <div onClick={() => setStartPageOpen(true)} className="start-button flex items-center justify-center w-full py-4 bg-primary text-white rounded-lg transition duration-300 capitalize font-bold text-xl cursor-pointer">
           START
@@ -110,22 +147,20 @@ export default function Home() {
       <div className="cancel-button flex justify-center items-center self-start w-10 h-10" onClick={() => setStartPageOpen(false)}>
         <Image src={"/icon/cancel_dark.svg"} alt="cancel" width={48} height={48} />
       </div>
-      <div className="h-24">
-        <div>{finalIndex !== -1 ? situation_data[finalIndex].title : situation_data[currentIndex].title}</div>
-      </div>
-      <div className="description text-center text-xl mt-4">
+      {randomContainer}
+      <div className="description text-center text-lg mb-2">
         <div className="description-text">이제부터 자기소개가 필요한</div>
         <div className="description-text"><span className="font-bold">랜덤의 상황이 제시</span>됩니다.</div>
         <div className="description-text mt-4">당신의 센스를 발휘해 보세요!</div>
       </div>
-      <div className="button-group flex flex-col items-center justify-center w-full pb-6">
-        <div onClick={() => handleResponseButtonClick("voice")} className="speak-button flex items-center justify-center w-full py-4 bg-black text-white rounded-lg transition duration-300 capitalize font-bold text-xl cursor-pointer">
+      <div className="button-group flex flex-col items-center justify-center w-full pb-6 text-lg">
+        <div onClick={() => handleResponseButtonClick("voice")} className="speak-button flex items-center justify-center w-full py-4 bg-black text-white rounded-lg transition duration-300 capitalize font-semibold cursor-pointer">
           <div className="icon flex items-center justify-center w-4 h-4 mr-2">
             <Image src={"/icon/mic.svg"} alt="mic" width={32} height={32} />
           </div>
           <div className="text">말로 답변</div>
         </div>
-        <div onClick={() => handleResponseButtonClick("text")} className="text-button flex items-center justify-center w-full py-3 mt-4 text-black rounded-lg transition duration-300 text-xl cursor-pointer">
+        <div onClick={() => handleResponseButtonClick("text")} className="text-button flex items-center justify-center w-full py-3 mt-4 text-black rounded-lg transition duration-300 cursor-pointer">
           <div className="icon flex items-center justify-center w-6 h-6 mr-2">
             <Image src={"/icon/bubble.svg"} alt="keyboard" width={32} height={32} />
           </div>
